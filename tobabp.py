7edd0b837e35aa374d78341531d8779ba1da52d6
Matthew Motz <mmotz@e-motz.com>
2015-08-29 16:02:32 -0400

    Update tobabp.py

--- tobabp.py
+++ tobabp.py
@@ -1,20 +1,5 @@
 #!/usr/bin/env python
-#  Copyright (C) 2012 Michael Clemens
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>
-
-import os
+import os, fnmatch
 import pyudev
 
 from mpd import (MPDClient, CommandError)
@@ -28,6 +13,13 @@ CON_ID = {'host':HOST, 'port':PORT}
 USBNAME = '5C04-57E8'
 
 # Some functions
+def find_files(directory, pattern):
+        for root, dirs, files in os.walk(directory):
+                for basename in files:
+                        if fnmatch.fnmatch(basename, pattern):
+                                filename = os.path.join(root, basename)
+                                yield filename
+
 def mpdConnect(client, con_id):
         try:
                 client.connect(**con_id)
@@ -37,8 +29,11 @@ def mpdConnect(client, con_id):
 def loadMusic(client, con_id, device):
         os.system("mount "+device+" /music/usb")
         os.system("/etc/init.d/mpd stop")
-        os.system("cp /music/usb/* /music/mp3/")
+        os.system("cp --no-clobber /music/usb/* /music/mp3/")
         os.system("umount /music/usb")
+        for filename in find_files('/music/mp3', '*.trash'):
+                os.remove(os.path.splitext(filename)[0])
+                os.remove(filename)
         os.system("rm /music/mpd/tag_cache")
         os.system("/etc/init.d/mpd start")
         os.system("mpc clear")
@@ -52,6 +47,7 @@ def checkForUSBDevice(name):
                 if device.get('ID_FS_LABEL') == name:
                         res = device.device_node
         return res
+
 def main():
         # MPD object instance
         client = MPDClient()
