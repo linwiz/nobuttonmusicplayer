9cb80f6003c6f64672936960e20c224c3a4e649d
Matthew Motz <mmotz@e-motz.com>
2015-09-07 14:22:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,7 @@ mkdir -p /music/{usb,mp3}
 
 cd /usr/local/bin
 wget http://git.io/vZINO
-wget http://git.io/vZINC
-wget http://git.io/vZINg
-wget http://git.io/vZINr
-chmod +x mocp-* tobabp.py
+chmod +x tobabp.py
 
 crontab -l > mycron
 echo "@reboot python /usr/local/bin/tobabp.py &" >> mycron
