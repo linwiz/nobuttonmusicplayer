ba57e3134a8f7ae9090edf88dd731d2864eeb9a7
linwiz <mmotz@e-motz.com>
2015-09-17 13:40:13 -0400

    Code cleanup

--- usr/local/bin/tnbmp.c
+++ usr/local/bin/tnbmp.c
@@ -1,36 +1,3 @@
-/*******************************************
-    ORIGINAL AUTHOR-PROVIDED INFORMATION
-********************************************
-
- libudev example.
-
- This example prints out properties of
- each of the hidraw devices. It then
- creates a monitor which will report when
- hidraw devices are connected or removed
- from the system.
-
- This code is meant to be a teaching
- resource. It can be used for anyone for
- any reason, including embedding into
- a commercial product.
-
- The document describing this file, and
- updated versions can be found at:
-    http://www.signal11.us/oss/udev/
-
- Alan Ott
- Signal 11 Software
- 2010-05-22 - Initial Revision
- 2010-05-27 - Monitoring initializaion
-              moved to before enumeration.
-*******************************************
-
-*******************************************
-
-
-
-*******************************************/
 #include <linux/limits.h>
 #include <libudev.h>
 #include <stdio.h>
@@ -43,18 +10,17 @@
 #include <sys/mount.h>
 #include <syslog.h>
 
-#define COMMAND_LEN 8
-#define DATA_SIZE 512
-
 int mypopen (char *command, char *mode)
 {
 	FILE *in;
 	extern FILE *popen();
 	char buff[512];
-	if(!(in = popen(command, mode))){
+	if (!(in = popen(command, mode)))
+	{
 		syslog (LOG_NOTICE, "Command failed: %s", command);
 	}
-	while(fgets(buff, sizeof(buff), in)!=NULL){
+	while (fgets(buff, sizeof(buff), in) != NULL)
+	{
 		syslog (LOG_NOTICE, "%s", buff);
 	}
 	pclose(in);
@@ -65,30 +31,33 @@ int main (void)
 {
 	setlogmask (LOG_UPTO (LOG_NOTICE));
 	openlog ("tnbmp", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-	syslog (LOG_NOTICE, "Program started by User %d", getuid ());
 
-	mypopen("su - pi -c \"/usr/bin/mocp -S\" 2>&1", "r");
-
-        if (geteuid()!=0)
+	if (geteuid() != 0)
 	{
 		syslog (LOG_NOTICE, "Please run tnbmp as a superuser");
 		closelog ();
-                return 1;
-        }
+		return 1;
+	}
+
+	syslog (LOG_NOTICE, "Program started by User %d", getuid ());
+	mypopen("su - pi -c \"/usr/bin/mocp -S\" 2>&1", "r");
+
 	const char* media_src = "/music/usb/";
 	const char* media_trgt = "/music/mp3/";
 	char command3[100];
-	sprintf(command3, "su - pi -c \"/bin/ls -d %s*.* > /home/pi/.moc/playlist.m3u && /usr/bin/mocp -o s,r,n -p\" 2>&1",media_trgt);
-	mypopen(command3,"r");
-
 	struct udev *udev;
 	struct udev_enumerate *enumerate;
 	struct udev_list_entry *devices, *dev_list_entry;
 	struct udev_device *dev;
 	char mysubs[] = "block";
 	char mysubs2[] = "partition";
-   	struct udev_monitor *mon;
+	struct udev_monitor *mon;
 	int fd;
+
+	sprintf(command3, "su - pi -c \"/bin/ls -d %s*.* > /home/pi/.moc/playlist.m3u"
+		" && /usr/bin/mocp -o s,r,n -p\" 2>&1", media_trgt);
+	mypopen(command3, "r");
+
 	udev = udev_new();
 	if (!udev)
 	{
@@ -96,6 +65,7 @@ int main (void)
 		closelog ();
 		exit(1);
 	}
+
 	mon = udev_monitor_new_from_netlink(udev, "udev");
 	udev_monitor_filter_add_match_subsystem_devtype(mon, mysubs, NULL);
 	udev_monitor_enable_receiving(mon);
@@ -104,18 +74,21 @@ int main (void)
 	udev_enumerate_add_match_subsystem(enumerate, mysubs2);
 	udev_enumerate_scan_devices(enumerate);
 	devices = udev_enumerate_get_list_entry(enumerate);
+
 	udev_list_entry_foreach(dev_list_entry, devices)
 	{
 		const char *path;
 		path = udev_list_entry_get_name(dev_list_entry);
 		dev = udev_device_new_from_syspath(udev, path);
-		dev = udev_device_get_parent_with_subsystem_devtype(dev,"usb","usb_device");
+		dev = udev_device_get_parent_with_subsystem_devtype(dev,
+							"usb", "usb_device");
 		if (!dev)
 		{
 			syslog (LOG_NOTICE, "Unable to find Parent usb device");
 		}
 	}
 	udev_enumerate_unref(enumerate);
+
 	while (1)
 	{
 		fd_set fds;
@@ -126,6 +99,7 @@ int main (void)
 		tv.tv_sec = 0;
 		tv.tv_usec = 0;
 		ret = select(fd+1, &fds, NULL, NULL, &tv);
+
 		if (ret > 0 && FD_ISSET(fd, &fds))
 		{
 			dev = udev_monitor_receive_device(mon);
@@ -141,26 +115,30 @@ int main (void)
 				reti = regexec(&regex, udev_device_get_devnode(dev), 0, NULL, 0);
 				if (!reti)
 				{
-					if (strcmp(udev_device_get_action(dev),"add")==0)
+					if (strcmp(udev_device_get_action(dev), "add") == 0)
 					{
-						syslog (LOG_NOTICE, "Device node added: %s", udev_device_get_devnode(dev));
+						syslog (LOG_NOTICE, "Device node added: %s",
+							udev_device_get_devnode(dev));
 						const char* mount_src  = udev_device_get_devnode(dev);
 						const char* mount_trgt = "/music/usb";
 						const char* mount_type = "vfat";
 						const unsigned long mount_flags = 0;
-						if (mkdir(media_trgt,0777)<0)
+
+						if (mkdir(media_trgt, 0777) < 0)
 						{
-							syslog (LOG_NOTICE, "Unable to create directory %s (already exists?)",media_trgt);
+							syslog (LOG_NOTICE, "Unable to create directory %s (already exists?)",
+								media_trgt);
 						}
-						if (mkdir(media_src,0777)<0)
+						if (mkdir(media_src, 0777) < 0)
 						{
-							syslog (LOG_NOTICE, "Unable to create directory %s (already exists?)",media_src);
+							syslog (LOG_NOTICE, "Unable to create directory %s (already exists?)",
+								media_src);
 						}
-						int result = mount(mount_src,mount_trgt,mount_type,mount_flags,NULL);
-						if (result==0)
+						int result = mount(mount_src, mount_trgt, mount_type, mount_flags, NULL);
+						if (result == 0)
 						{
 							mypopen("su - pi -c \"/usr/bin/mocp -s\" 2>&1", "r");
-							syslog (LOG_NOTICE, "Mount created at %s...\n",mount_trgt);
+							syslog (LOG_NOTICE, "Mount created at %s...\n", mount_trgt);
 							syslog (LOG_NOTICE, "Removing old music...\n");
 							mypopen("ls -lah /music/mp3 2>&1", "r");
 							char command1[100];
@@ -169,7 +147,7 @@ int main (void)
 							mypopen("ls -lah /music/mp3 2>&1", "r");
 							syslog (LOG_NOTICE, "Adding new music...\n");
 							char command2[100];
-							sprintf(command2, "cp -v %s*.* %s 2>&1",media_src,media_trgt);
+							sprintf(command2, "cp -v %s*.* %s 2>&1", media_src, media_trgt);
 							mypopen(command2, "r");
 							umount(mount_trgt);
 							syslog (LOG_NOTICE, "Rebuilding playlist...\n");
@@ -178,7 +156,8 @@ int main (void)
 						}
 						else
 						{
-							syslog (LOG_NOTICE, "Error : Failed to mount %s\n Reason: %s [%d]\n",mount_src,strerror(errno),errno);
+							syslog (LOG_NOTICE, "Error : Failed to mount %s\n Reason: %s [%d]\n",
+								mount_src, strerror(errno), errno);
 						}
 						udev_device_unref(dev);
 					}
@@ -186,7 +165,7 @@ int main (void)
 				regfree(&regex);
 			}
 		}
-		usleep(250*1000);
+		usleep(250 * 1000);
 		fflush(stdout);
 	}
 	udev_unref(udev);
